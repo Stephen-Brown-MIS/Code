df4f136aa11e76dca388e45127208cea39d4d080
Stephen Brown <119617707+Stephen-Brown-MIS@users.noreply.github.com>
2023-04-21 09:57:25 -0400

    Algo Assignments

--- Algos/P&A/StringsToDo1.js
+++ Algos/P&A/StringsToDo1.js
@@ -1,87 +1,147 @@
-// Push Front
-// Given an array and an additional value, insert this value at the beginning of the array. You may use .push(), you are able do this without it though!
+// Remove Blanks
+// Create a function that, given a string, returns all of that string’s contents, but without blanks. 
 
 // Examples:
 
-// pushFront([5,7,2,3], 8) => [8,5,7,2,3]
-// pushFront([99], 7) => [7,99]
+// removeBlanks(" Pl ayTha tF u nkyM usi c ") => "PlayThatFunkyMusic"
+// removeBlanks("I can not BELIEVE it's not BUTTER") => "IcannotBELIEVEit'snotBUTTER"
 
-var newArray = [];
-var pushValue = 1;
+var new_String = "";
 
-function pushFront(array, pushValue) {
-    newArray.push(pushValue);
-    for (var i = 0; i <= array.length - 1; i++) {
+function removeBlanks(string) {
+    
+       for(var i = 0; i < string.length; i++){
+            if (string[i] != ' '){
+            new_String += string[i]
+        }
+    }
+    console.log (new_String)
+    return new_String
+    
+}
+
+//removeBlanks(" Pl ayTha tF u nkyM usi c ")
+removeBlanks("I can not BELIEVE it's not BUTTER")
+
+//----------------------------------------------------------------------------------------------------
+
+
+// Get Digits
+// Create a JavaScript function that given a string, returns the integer made from the string’s digits. 
+// You are allowed to use isNaN() and Number().
+
+// Examples:
+
+// getDigits("abc8c0d1ngd0j0!8") => 801008
+
+// getDigits("0s1a3y5w7h9a2t4?6!8?0") => 1357924680
+
+var new_String = "";
+
+function getDigits(string) {
+    for(var i = 0; i < string.length; i++){
+            if (!isNaN(string[i])) {
+            new_String += string[i]
+        }
+    }
+    console.log (new_String)
+    return new_String
+}
+
+//getDigits("abc8c0d1ngd0j0!8") 
+
+ getDigits("0s1a3y5w7h9a2t4?6!8?0") 
+
+
+//------------------------------------------------------------------------------------------------------------------
+
+
+// Acronyms
+// Create a function that, given a string, returns the string’s acronym (first letter of the word capitalized). 
+// You are allowed to use .split() and .toUpperCase().
+
+// acronym(" there's no free lunch - gotta pay yer way. ") => "TNFL-GPYW". 
+
+// acronym("Live from New York, it's Saturday Night!") => "LFNYISN".
+
+
+function acronym(str) {
+    new_acronym = "";
+    word = false;
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == " ") {
+        word = false;
+        continue;
+      }
+      if (word == false) {
+        new_acronym += str[i].toUpperCase();
+        word = true;
+      }
+      else {
+        continue;
+      }
+    }
+    console.log(new_acronym)
+    return new_acronym
+  }
+
+acronym(" there's no free lunch - gotta pay yer way. ")
+
+//  acronym("Live from New York, it's Saturday Night!")
+
+//-------------------------------------------------------------------------------------------------------------------
+
+// Count Non-Spaces
+// Create a function that, given a string, returns the number of non-space characters found in the string. 
+
+// Examples:
+
+// countNonSpaces("Honey pie, you are driving me crazy") => 29
+// countNonSpaces("Hello world !") => 11
+
+function countNonSpaces(str) {
+    count = 0;
+    
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] != " ") {
+        count+=1;
+      }
+    }
+    console.log(count)
+    return count
+  }
+
+//  countNonSpaces("Honey pie, you are driving me crazy") 
+ countNonSpaces("Hello world !")
+
+//------------------------------------------------------------------------------------------------------------------
+
+// Remove Shorter Strings
+// Create a function that, given an array of strings and a numerical value, returns an array 
+// that only contains strings longer than or equal to the given value.
+
+// Examples:
+
+// removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4) => ['Good morning', 'sunshine', 'Earth', 'says', 'hello']
+// removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3) => ['There', 'bug', 'the', 'system']
+
+newArray =[];
+
+function removeShorterStrings(array, size) {
+    count = 0;
+    
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].length >= size) {
         newArray.push(array[i]);
+      }
     }
-}
-//pushFront([5,7,2,3], 8);
-pushFront([99], 7)
-console.log(newArray);
+    console.log(newArray)
+    return newArray
+  }
 
-//---------------------------------------------------------
+// removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4)
+removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3)
 
-// Pop Front
-// Given an array, remove and return the value at the beginning 
-// of the array. Prove the value is removed from the array 
-// by printing it. 
-// You may use .pop(), you are able do this without it though!
-
-// Examples:
-
-// popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
-// popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
-
-var newArray = [];
-var popNum = 1;
-
-function popFront(array) {
-    popNum = array[0]
-    
-    for (var i = 1; i <= array.length - 1; i++) {
-        newArray[i-1]=array[i];
-    }
-    console.log(newArray);
-    console.log(popNum);
-    return popNum
-
-}
-
-
-// popFront([0,5,10,15])
-popFront([4,5,7,9])
-
-//---------------------------------------------------------------
-
-// Insert At
-// Given an array, index, and additional value, insert the value into 
-// array at given index. You can think of pushFront(arr,val) 
-// as equivalent to insertAt(arr,0,val). 
-// You may use .push(), you are able do this without it though!
-
-// Examples:
-
-// insertAt([100,200,5], 2, 311) => [100,200,311,5]
-// insertAt([9,33,7], 1, 42) => [9,42,33,7]
-
-
-var newArray = [];
-var popNum = 1;
-
-function insertAt(array,index, num1) {
-    popNum = array[0]
-    
-    for (var i = 0; i <= array.length - 1; i++) {
-        if (i < index){
-            newArray[i]=array[i];
-        }
-        else if (i == index){
-            newArray[i] = num1
-        }
-        newArray[i+1]=array[i];
-    }
-    console.log(newArray);
-}
-
-//  insertAt([100,200,5], 2, 311) 
- insertAt([9,33,7], 1, 42)
